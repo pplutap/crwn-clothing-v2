88d6e731b1979ab2727fd939a5198408c641690c
Pawel Pluta <pluta.p@gmail.com>
2023-01-30 23:29:48 +0100

    Checkout and cart components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,14 @@
 import "./product-card.styles.scss";
 
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
+
   const { name, price, imageUrl } = product;
   return (
     <div className="product-card-container">
@@ -11,7 +17,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
